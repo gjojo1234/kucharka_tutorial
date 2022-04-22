c3a863ee3bc6012ca3288cf7f9ce0a0f2248b8c8
gjojo1234 <gensor1234@gmail.com>
2022-04-22 19:52:50 +0200

    vycistenie od prebytocneho kodu

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const mongo = require('./database/mongo');
-// const dotenv = require('dotenv');
-// const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const getMaterial = require('./routes/GET/getMaterial');
 const saveMaterial = require("./routes/POST/saveMaterial");
 
@@ -13,20 +12,11 @@ const connectToMongoDB = async () => {
   await mongo().then( async(mongoose) => {
     try {console.log("connected to mongodb")}
     catch{console.log('nepripojene do db')}
-    //finally {mongoose.connection.close()}
+    
   })
 }
 connectToMongoDB();
 
-/*
-dotenv.config();
-const client = new MongoClient(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-*/
 
 //povolenie prijimania z frontendu (middleweare) - musi byt nad ostatnymi use
 
